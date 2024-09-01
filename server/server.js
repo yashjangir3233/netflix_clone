77cea1a2a771637d1e02f35706d0433f55bf12bc
yashHindgreco <103257851+yashjangir3233@users.noreply.github.com>
2024-09-01 21:52:27 +0530

    test

--- server/server.js
+++ server/server.js
@@ -23,14 +23,15 @@ app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/movie", protectRoute, movieRoutes);
 app.use("/api/v1/tv", protectRoute, tvRoutes);
 app.use("/api/v1/search", protectRoute, searchRoutes);
+console.log(ENV_VARS.NODE_ENV)
 
-if (ENV_VARS.NODE_ENV === "production") {
+// if (ENV_VARS.NODE_ENV === "production") {
 	app.use(express.static(path.join(__dirname, "/client/dist")));
 
 	app.get("*", (req, res) => {
 		res.sendFile(path.resolve(__dirname, "client", "dist", "index.html"));
 	});
-}
+// }
 
 app.listen(PORT, () => {
 	console.log("Server started at http://localhost:" + PORT);
