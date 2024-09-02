b596f1d0597cdccfe65e360a56cac6f1fd632324
yashHindgreco <103257851+yashjangir3233@users.noreply.github.com>
2024-09-02 10:48:51 +0530

    test

--- client/vite.config.js
+++ client/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
 	server: {
 		proxy: {
 			"/api": {
-				target: "http://localhost:5000",
+				target: "https://netflix-clone-opal-sigma-59.vercel.app/",
 			},
 		},
 	},
